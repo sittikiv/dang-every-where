973688b4705cb57e6ab61fa2668c621d71204c81
NTBG\Sittiporn.Lek <sittiporn.lek@ntb.co.th>
2023-07-20 17:16:20 +0700

    update path

--- src/content.js
+++ src/content.js
@@ -1,7 +1,7 @@
 let searchTimeOut = () => {}
 // const test = chrome.runtime.getURL('eiei.gif')
 // console.log(test)
-const imgSrc = chrome.runtime.getURL('assets/image/eiei.gif')
+const imgSrc = 'https://media.tenor.com/UJSaelt_l0MAAAAd/%E0%B8%AD%E0%B8%B2%E0%B8%88%E0%B8%B2%E0%B8%A3%E0%B8%A2%E0%B9%8C%E0%B9%81%E0%B8%94%E0%B8%87.gif'
 function ready() {
   const element = document.getElementsByTagName("img");
   clearTimeout(searchTimeOut)
